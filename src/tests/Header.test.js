e20437ae97af2bf1f4ad31a1286eafc8999ec2c7
caiobacode <caioodsf@gmail.com>
2022-10-24 20:24:06 -0300

    tirando browser router do app

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -9,28 +9,24 @@ describe('Header test', () => {
   it('Drinks test', () => {
     const { history } = renderWithRouter(<App />);
     act(() => history.push('/drinks'));
-    console.log(history.location.pathname);
     const linkElement = screen.getByTestId(pageTest);
     expect(linkElement).toBeInTheDocument();
   });
   it('Done Recipes test', () => {
     const { history } = renderWithRouter(<App />);
     act(() => history.push('/done-recipes'));
-    console.log(history.location.pathname);
     const linkElement = screen.getByTestId(pageTest);
     expect(linkElement).toBeInTheDocument();
   });
   it('Favorite Recipes test', () => {
     const { history } = renderWithRouter(<App />);
     act(() => history.push('/favorite-recipes'));
-    console.log(history.location.pathname);
     const linkElement = screen.getByTestId(pageTest);
     expect(linkElement).toBeInTheDocument();
   });
   it('Profile test', () => {
     const { history } = renderWithRouter(<App />);
     act(() => history.push('/profile'));
-    console.log(history.location.pathname);
     const linkElement = screen.getByTestId(pageTest);
     expect(linkElement).toBeInTheDocument();
   });
