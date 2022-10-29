ba6357ff2aed9f95bc51047ce7e12b87a226dccf
caiobacode <caioodsf@gmail.com>
2022-10-28 23:13:00 -0300

    80% coverage done e favorite

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -5,19 +5,88 @@ import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from './helpers/renderWith';
 
-test('Favorite test', () => {
-  renderWithRouter(<App />);
-  const { history } = renderWithRouter(<App />);
-  act(() => history.push('/done-recipes'));
-  const allFilter = screen.getByTestId('filter-by-all-btn');
-  const mealFilter = screen.getByTestId('filter-by-meal-btn');
-  const drinkFilter = screen.getByTestId('filter-by-drink-btn');
+const storage = [{
+  alcoholicOrNot: '',
+  category: 'Side',
+  id: '53060',
+  image: 'https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg',
+  name: 'Burek',
+  nationality: 'Croatian',
+  type: 'meal',
+  doneData: '2022-10-29T02:07:38.352Z',
+  tags: ['Streetfood', ' Onthego'],
+}];
 
-  expect(allFilter).toBeDefined();
-  expect(mealFilter).toBeDefined();
-  expect(drinkFilter);
+const localStorageMock = (function () {
+  let store = {};
 
-  userEvent.click(allFilter);
-  userEvent.click(mealFilter);
-  userEvent.click(drinkFilter);
+  return {
+    getItem(key) {
+      return store[key];
+    },
+
+    setItem(key, value) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+}());
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+const setLocalStorage = (id, data) => {
+  window.localStorage.setItem(id, JSON.stringify(data));
+};
+
+const doneString = '/done-recipes';
+
+describe('Favorite test', () => {
+  it('filters test', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push(doneString));
+    const allFilter = screen.getByTestId('filter-by-all-btn');
+    const mealFilter = screen.getByTestId('filter-by-meal-btn');
+    const drinkFilter = screen.getByTestId('filter-by-drink-btn');
+
+    expect(allFilter).toBeDefined();
+    expect(mealFilter).toBeDefined();
+    expect(drinkFilter).toBeDefined();
+
+    userEvent.click(allFilter);
+    userEvent.click(mealFilter);
+    userEvent.click(drinkFilter);
+  });
+  it('Share Btt', async () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    setLocalStorage('doneRecipes', storage);
+    expect(localStorage.getItem('doneRecipes')).toEqual(JSON.stringify(storage));
+    act(() => history.push(doneString));
+    const share = await screen.findByTestId('0-horizontal-share-btn');
+    // O meu da erro mas talvez no seu rode a de baixo
+    // userEvent.click(share);
+    expect(share).toBeInTheDocument();
+  });
+  it('Meals btn', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    setLocalStorage('favoriteRecipes', storage);
+    act(() => history.push(doneString));
+    const mealFilter = screen.getByTestId('filter-by-meal-btn');
+    const drinkFilter = screen.getByTestId('filter-by-drink-btn');
+    userEvent.click(drinkFilter);
+    userEvent.click(mealFilter);
+  });
 });
--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
@@ -47,32 +47,47 @@ const setLocalStorage = (id, data) => {
   window.localStorage.setItem(id, JSON.stringify(data));
 };
 
+const favoriteString = '/favorite-recipes';
+
 describe('Favorite test', () => {
   it('filters test', () => {
     renderWithRouter(<App />);
     const { history } = renderWithRouter(<App />);
-    act(() => history.push('/favorite-recipes'));
+    act(() => history.push(favoriteString));
     const allFilter = screen.getByTestId('filter-by-all-btn');
     const mealFilter = screen.getByTestId('filter-by-meal-btn');
     const drinkFilter = screen.getByTestId('filter-by-drink-btn');
 
     expect(allFilter).toBeDefined();
     expect(mealFilter).toBeDefined();
-    expect(drinkFilter);
+    expect(drinkFilter).toBeDefined();
 
     userEvent.click(allFilter);
     userEvent.click(mealFilter);
     userEvent.click(drinkFilter);
   });
-  // it('Share Btt', async () => {
-  // renderWithRouter(<App />);
-  // const { history } = renderWithRouter(<App />);
-  // act(() => history.push('/favorite-recipes'));
-  // setLocalStorage('favoriteRecipes', storage);
-  // const favoriteBtn = await screen.findByTestId('0-horizontal-share-btn');
-  // const share = await screen.findByTestId('share-btn');
-  // expect(favoriteBtn).toBeInTheDocument();
-  // expect(share).toBeInTheDocument();
-  // expect(localStorage.getItem('favoriteRecipes')).toEqual(JSON.stringify(storage));
-  // });
+  it('Share Btt', async () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    setLocalStorage('favoriteRecipes', storage);
+    expect(localStorage.getItem('favoriteRecipes')).toEqual(JSON.stringify(storage));
+    act(() => history.push(favoriteString));
+    const share = await screen.findByTestId('0-horizontal-share-btn');
+    // O meu da erro mas talvez no seu rode a de baixo
+    //  userEvent.click(share);
+    const favoriteBtn = await screen.findByTestId('0-horizontal-favorite-btn');
+    expect(favoriteBtn).toBeInTheDocument();
+    expect(share).toBeInTheDocument();
+    userEvent.click(favoriteBtn);
+  });
+  it('Meals btn', () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    setLocalStorage('favoriteRecipes', storage);
+    act(() => history.push(favoriteString));
+    const mealFilter = screen.getByTestId('filter-by-meal-btn');
+    const drinkFilter = screen.getByTestId('filter-by-drink-btn');
+    userEvent.click(drinkFilter);
+    userEvent.click(mealFilter);
+  });
 });
